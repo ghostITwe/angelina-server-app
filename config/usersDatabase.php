6bd201b0974da1e43d7979ace0b4e09cc6a8d905
Vladislav <vladislav.ilyinr@gmail.com>
2022-05-11 16:22:41 +0300

    add users for userDatabase.php

--- config/usersDatabase.php
+++ config/usersDatabase.php
@@ -1,11 +1,41 @@
 <?php
 
 return [
-    'test' => [
-        'host' => '172.20.8.',
+    'iggfycdy' => [
+        'host' => '172.20.8.19',
         'database' =>  'test',
-        'username' => 'testName',
-        'password' => 'testPassword',
+        'username' => 'iggfycdy',
+        'password' => '2Sz4rh',
+    ],
+    'pdgvjgrn' => [
+        'host' => '172.20.8.19',
+        'database' =>  'test',
+        'username' => 'pdgvjgrn',
+        'password' => 'hJUCcD',
+    ],
+    'jgscbscb' => [
+        'host' => '172.20.8.19',
+        'database' =>  'test',
+        'username' => 'jgscbscb',
+        'password' => 'msenx5',
+    ],
+    'yrvrghdy' => [
+        'host' => '172.20.8.19',
+        'database' =>  'test',
+        'username' => 'yrvrghdy',
+        'password' => 'fTHNiA',
+    ],
+    'jbstzhxn' => [
+        'host' => '172.20.8.19',
+        'database' =>  'test',
+        'username' => 'jbstzhxn',
+        'password' => 'MThhAi',
+    ],
+    'jyhdpnjy' => [
+        'host' => '172.20.8.19',
+        'database' =>  'test',
+        'username' => 'jyhdpnjy',
+        'password' => 'iDxxNF',
     ],
     'default' => [
         'host' => '127.0.0.1',
